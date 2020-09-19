2cb31c92ad8ccf3fb3b5fe95cb56813a2b7a8652
Mike Lee <rockespecialee@hotmail.com>
2020-09-19 14:39:45 -0700

    enemy and player collision

--- KimBot/Assets/enemyScript.cs
+++ KimBot/Assets/enemyScript.cs
@@ -7,9 +7,9 @@ public class enemyScript : MonoBehaviour
 
     public Transform player;
     public Vector2 enemyVector;
-
+    private float distance;
     public Rigidbody2D rb;
-
+    public bool collided = false;
 
 
 
@@ -26,6 +26,12 @@ public class enemyScript : MonoBehaviour
         enemyVector = new Vector2(player.transform.position.x - gameObject.transform.position.x, player.transform.position.y - gameObject.transform.position.y);
         enemyVector.Normalize();
         transform.Translate(enemyVector * Time.deltaTime * 2);
-        Debug.Log(enemyVector);
+
+        distance = Vector3.Distance(player.transform.position, gameObject.transform.position);
+        //player.gameObject.GetComponent<SpriteRenderer>().size.x
+        if (distance <=  2.1) {
+            collided = true;
+            Debug.Log("hit");
+        }
     }
 }

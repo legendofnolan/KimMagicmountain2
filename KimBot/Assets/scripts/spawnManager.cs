1d5687e857fd6e8b82f7cb4238b1a9b0aa036a1c
Nolan Ohlheiser <legendofnolan@gmail.com>
2020-09-19 17:29:23 -0700

    almost spawn manager

---
+++ KimBot/Assets/scripts/spawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnManager : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    //public GameObject enemyPrefab;
+
+    public float intervalWave;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InvokeRepeating("spawnObject", 1.0f, intervalWave);
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(enemyPrefab, enemyPrefab.transform.position, enemyPrefab.transform.rotation);
+    }
+}
